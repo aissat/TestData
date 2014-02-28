f60d18f9b1a6d12fa75ca10d62caa1e6f6111bde
aissat <mr.aissat@gmail.com>
2014-02-28 01:45:21 +0100

    commit2
    update index.php

--- index.php
+++ index.php
@@ -1,67 +1,85 @@
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+﻿<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<?php
+/*
+$doc = new DOMDocument;
+@$doc->loadhtmlfile('index5.html');
+
+$p = $doc->getElementsByTagName('p');
+if ( $p && 0<$p->length ) {
+	// وجدت أن الفقرة التي نريدها ترتيبها الرابع
+    $p = $p->item(3);
+    echo $doc->savehtml($p);
+    // اذا أردت حذف  الوسوم
+    //echo strip_tags($doc->savehtml($p));
+}*/
+?>
+
 <?php
 
 
 function Readhtml()
 {
-	# code...
+# code...
 
-	@$doc->loadhtmlfile('index.html');
-	//@$doc->loadhtmlfile($value);
-	//echo 1;
-	$p = $doc->getElementsByTagName('p');
-	if ( $p && 0<$p->length ) {
-		// وجدت أن الفقرة التي نريدها ترتيبها الرابع
-	    $p = $p->item(3);
-	    echo $doc->savehtml($p);
-		echo 2;
-	    // اذا أردت حذف  الوسوم
-	    //echo strip_tags($doc->savehtml($p))
-	}
-	
+@$doc->loadhtmlfile('index.html');
+//@$doc->loadhtmlfile($value);
+//echo 1;
+$p = $doc->getElementsByTagName('p');
+if ( $p && 0<$p->length ) {
+// وجدت أن الفقرة التي نريدها ترتيبها الرابع
+$p = $p->item(3);
+echo $doc->savehtml($p);
+echo 2;
+// اذا أردت حذف الوسوم
+//echo strip_tags($doc->savehtml($p))
+}
+
 };
-
-
-
-
-
 
 function DirParc($path){
 
 	$_dir = opendir($path) ;
 	while(false !==($_file= readdir($_dir))){
 		if (is_dir($path.$_file) &&  $_file!=".." &&  $_file!="."  ) {
-			print $_file;
+			//print $_file;
 			//Readhtml($_file);
 			DirParc($path.$_file.'/');
 		} else if(is_file($path.$_file) && $_file!=".." &&  $_file!="."   )
 		{ 
-			print $_file."<br/>";
+			//print $_file."<br/>";
 			//echo 0;
 			$doc = new DOMDocument;
-			@$doc->loadhtmlfile('index6.html');
-			//echo 1;		
+			@$doc->loadhtmlfile($path.$i.'/index.html');
 			$p = $doc->getElementsByTagName('p');
-			//echo 2;		
 			if ( $p && 0 < $p->length ) {
-				// وجدت أن الفقرة التي نريدها ترتيبها الرابع
-				
-				$p = $p->item(3);
-				//echo 3;
-				echo $doc->savehtml($p);
-				//echo 4;
-				
-				// اذا أردت حذف  الوسوم
-				//echo strip_tags($doc->savehtml($p))
-			}			
+			$p = $p->item(3);
+			echo $doc->savehtml($p);			
 		}
 	};
 }
-//   'pan/' هذا مجلد html 
+}
 DirParc('pan/');
 
-//Readhtml('index.html');
 
 
+/*
+function Dirr($path){
+	
+$_dir = opendir($path) ;
+$i = 0;
+while(false !==($_file= readdir($_dir))){
+if (is_dir($path.$_file) && $_file!=".." && $_file!="." ) 
+	$i++;
+		$doc = new DOMDocument;
+		@$doc->loadhtmlfile($path.$i.'/index.html');
+		$p = $doc->getElementsByTagName('p');
+		if ( $p && 0 < $p->length ) {
+		$p = $p->item(3);
+		echo $doc->savehtml($p);
+		}	
+	}
+}
 
+Dirr('pan/');
+*/
 ?>
